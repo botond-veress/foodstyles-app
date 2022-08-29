f66fe575a462a6bab15c48bac60b02934d7db4dd
Botond Veress <botond.veress@gmail.com>
2022-08-29 03:09:51 +0300

    feat: update app with routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
 import React from 'react';
 
+import { Session } from '@/context/session';
+
+import { GuestRoutes } from '@/components/App/GuestRoutes';
+import { AuthorizedRoutes } from '@/components/App/AuthorizedRoutes';
+
 export const App: React.FC = () => {
-  return null;
+  const { authenticated, loading } = Session.useContainer();
+
+  if (!authenticated && loading) return null;
+
+  if (!authenticated) return <GuestRoutes />;
+
+  return <AuthorizedRoutes />;
 };
